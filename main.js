5853b704d0ea56f515fa2ca53c609b51981180e3
thanarul <thanarul04@gmail.com>
2023-10-15 14:09:34 -0400

    improved the front page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+
 var typed = new Typed(".text", {
-    Strings:["Student" , "Programmer" , "Soccer Player"],
+    strings:["Student" , "Programmer" , "Soccer Player"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
