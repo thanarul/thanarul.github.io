dfb59cf4fd84aa29407b567e8c8695cc9dca4a2f
thanarul <thanarul04@gmail.com>
2024-08-23 14:52:07 -0400

    sizing changes to workterm page

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings:["Student" , "Programmer" , "Soccer Player"],
+    strings:["Student" , "Programmer" , "Soccer Player", "Software Engineer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
